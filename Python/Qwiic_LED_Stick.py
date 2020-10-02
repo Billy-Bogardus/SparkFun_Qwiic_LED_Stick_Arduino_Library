3aaedc413763827b6e94f554beb2e9ec1cf696a5
Billy Bogardus <billbogardus1@gmail.com>
2020-10-01 23:28:48 -0600

    Good QWIIC_LED_Stick Class Added Blink Extentions one watches STDIO, the other watches a file for the value False

--- Python/Qwiic_LED_Stick.py
+++ Python/Qwiic_LED_Stick.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import os
 import time
+import _thread
 
 #Module for Qwiic_LED_Stick
 class Qwiic_LED_Stick:
@@ -24,6 +25,38 @@ class Qwiic_LED_Stick:
         except:
             print("smbus not available. Please make sure this Module is installed.")
 
+    def changeLength(self, length):
+        self.bus.write_byte_data(self.LED_Stick_Address, self.COMMAND_CHANGE_LED_LENGTH, length)
+
+    def changeAddress(self, newAddress):
+        self.bus.write_byte_data(self.LED_Stick_Address, self.COMMAND_CHANGE_ADDRESS, newAddress)
+        self.LED_Stick_Address = newAddress
+        #Turn LEDs on and off so address can be changed back imeadiatly
+        self.allOn()
+        self.allOff()
+
+
+
+    #Change the brightness of a specific or all LED, while keeping their current color
+    #brightness must be a value between 0-31
+    #To turn LEDs off but remember their previous color, set brightness to 0
+    #LEDS indexed starting at 1
+    #number parameter is optional
+    def setLEDBrightness(self, brightness, number=0):
+        if(brightness > 31):
+            return False
+        if(number <= 0):
+            self.bus.write_byte_data(self.LED_Stick_Address, self.COMMAND_WRITE_ALL_LED_BRIGHTNESS,
+                brightness)
+            return True
+        else:
+            self.bus.write_i2c_block_data(self.LED_Stick_Address, self.COMMAND_WRITE_SINGLE_LED_BRIGHTNESS, 
+            [
+                number,
+                brightness
+            ])
+            return True
+
     #Write LED Color(s)
     def setLEDColor(self, red, green, blue, number=0):
         #set colors of LED's to individual Colors
@@ -45,32 +78,12 @@ class Qwiic_LED_Stick:
         else:
             print("Bad Arguments")
 
-    #Simple Blink Code with adjustable on and off delay
-    def blink(self, timeA = 0.5, timeB = 0.5):
-        while(1):
-            self.setLEDColor(0,0,0)
-            time.sleep(timeA)
-            self.setLEDColor(30,30,30,2)#TODO add configuration for this code
-            time.sleep(timeB)
-    
-    #Blink a rainbow TODO find proper color Values
-    def blinkRainbow(self, timeA = 0.5, timeB = 0.5):
-        while(1):
-            self.setLEDColor(0,0,0,0)
-            #       1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 12, 13, 14
-            rr = [148, 75,  0,  0,255,255,255,  0,  0,  0,  0]
-            rb = [  0,  0,  0,255,255,127, 10,  0,  0,  0,  0]
-            rg = [211,130,255,  0,  0,  0,  0,  0,  0,  0,  0]
-            time.sleep(timeA)
-            self.setLEDColor(rr,rb,rg)
-            time.sleep(timeB)
-
     #All LEDs on Full Blast
     def allOn(self):
-        self.setLEDColor(255,255,255,0)
+        self.setLEDColor(255,255,255)
     #All LEDs off
     def allOff(self):
-        self.setLEDColor(0,0,0,0)
+        self.bus.write_byte(self.LED_Stick_Address, self.COMMAND_WRITE_ALL_LED_OFF)
 
     #Write a Collor array
     def WriteColorArray(self, command, array):
@@ -92,7 +105,13 @@ class Qwiic_LED_Stick:
             data = [arrLen, n*12] + array[sliver] + [arrLen]
             
             self.bus.write_i2c_block_data(self.LED_Stick_Address, command, data)
-    
+
+    #Listen to keyboard 
+    def stdIOIn(self, TList):
+        a = input()
+        print(a)
+        TList.append(True)
+
     #arrays for use latter
     def holdgarbage(self):
             self.setLEDColor(
@@ -117,7 +136,92 @@ class Qwiic_LED_Stick:
                 #  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 12, 13, 14
             time.sleep(0.5)
 
+#Blinkes the LED's untill the enter key is pressed.
+class Qwiic_LED_Blink(Qwiic_LED_Stick):
+    #Simple Blink Code with adjustable on and off delay
+    def blink(self, timeA = 0.5, timeB = 0.5):
+        B_list = []
+        _thread.start_new_thread(self.stdIOIn, (B_list,))
+        while not B_list:
+            self.setLEDColor(0,0,0)
+            time.sleep(timeA)
+            self.allOn()
+            time.sleep(timeB)
+    
+    #Blink a rainbow TODO find proper color Values
+    def blinkRainbow(self, timeA = 0.5, timeB = 0.5):
+        B_list = []
+        _thread.start_new_thread(self.stdIOIn, (B_list,))
+        while not B_list:
+            self.setLEDColor(0,0,0,0)
+            #       1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 12, 13, 14
+            rr = [  0,  0,  0,148, 75,  0,  0,255,255,255,  0]
+            rb = [  0,  0,  0,  0,  0,  0,255,255,165, 10,  0]
+            rg = [  0,  0,  0,211,130,255,  0,  0,  0,  0,  0]
+            time.sleep(timeA)
+            self.setLEDColor(rr,rb,rg)
+            time.sleep(timeB)
 
-bling = Qwiic_LED_Stick(1)
-#bling.blink()
-bling.blinkRainbow()
+
+#class tracks a file and blinks as long as the first five letters do not == False
+class Qwiic_LED_Blink_File(Qwiic_LED_Stick):
+    #Simple Blink Code with adjustable on and off delay
+    def blink(self, timeA = 0.5, timeB = 0.5, filename="Python/text"):
+        B_list = []
+        _thread.start_new_thread(self.monitorFile, (filename, B_list,))
+        while not B_list:
+            self.setLEDColor(0,0,0)
+            time.sleep(timeA)
+            self.allOn()
+            time.sleep(timeB)
+    
+    #Blink a rainbow TODO find proper color Values
+    def blinkRainbow(self, timeA = 0.5, timeB = 0.5, filename="Python/text"):
+        B_list = []
+        _thread.start_new_thread(self.monitorFile, (filename, B_list,))
+        while not B_list:
+            self.setLEDColor(0,0,0,0)
+            #       1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 12, 13, 14
+            rr = [  0,  0,  0,148, 75,  0,  0,255,255,255,  0]
+            rb = [  0,  0,  0,  0,  0,  0,255,255,165, 10,  0]
+            rg = [  0,  0,  0,211,130,255,  0,  0,  0,  0,  0]
+            time.sleep(timeA)
+            self.setLEDColor(rr,rb,rg)
+            time.sleep(timeB)
+    #This function will be launched in a different thread and will monitor the specified file for a value of False
+    def monitorFile(self, filename, TList):
+        f = open(filename,"r")
+        val = True
+        while val:
+            text = f.read(5)
+            if(text == "False"):
+                val = False
+            f.seek(0)
+        f.close()
+        TList.append(True)
+    def resetFile(self, filename):
+        f = open(filename, "wt")
+        f.write("True")
+        f.close()
+
+# Test File based Loop
+if __name__ == '__main__':
+    print("Test Script for Qwiic_LED_Stick module")
+    print("Press Enter to exit")
+    #initialize module
+    bling = Qwiic_LED_Blink_File(1)
+    #set brightness to 1 I like my eyeballs
+    bling.setLEDBrightness(1)
+    #blink LED
+    bling.changeLength(10)
+    bling.resetFile("Python/text")
+    bling.blink()
+    #bling.allOn()
+    #time.sleep(0.5)
+    #bling.allOff()
+    #time.sleep(0.5)
+    #blink Rainbow
+    bling.resetFile("Python/text")
+    bling.blinkRainbow()
+    bling.resetFile("Python/text")
+    bling.allOff()
